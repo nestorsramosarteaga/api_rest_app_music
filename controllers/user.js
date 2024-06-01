d7d233ebc886f2f29abc4d8f9bfd9e87ec08057f
Nestor Ramos <nestorsramosarteaga@gmail.com>
2024-06-01 09:24:47 -0500

    Verifiy data in regsister User

--- controllers/user.js
+++ controllers/user.js
@@ -5,7 +5,32 @@ const testing = (req,res) => {
         message: "Message sent from user controller"
     });
 }
+
+// Register
+const register = (req, res) => {
+
+    // Collecting data
+    let params = req.body;
+
+    // Verify data
+    if(!params.name || !params.nick || !params.email || !params.password){
+        return res.status(400).send({
+            status: "error",
+            message: "Required data is missing"
+        });
+    }
+    // Validate data
+
+    return res.status(200).send({
+        status: "success",
+        message: "Register Method"
+    });
+
+}
+
+
 // export
 module.exports = {
-    testing
+    testing,
+    register
 }
