15370146a34fd57ecae0fbff4e495ccc321a7dba
Nestor Ramos <nestorsramosarteaga@gmail.com>
2024-06-01 09:13:41 -0500

    Create User Model

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const {Schema, model} = require("mongoose");
+
+const UserSchema = Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    surname: {
+        type: String
+    },
+    nick: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        default: "role_user"
+    },
+    image: {
+        type: String,
+        default: "default.png"
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = model("User", UserSchema, "users");
+                // collection: users
