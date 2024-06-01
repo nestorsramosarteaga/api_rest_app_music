648525ef5b987577b73e4b4dd32ac78a76a22614
Nestor Ramos <nestorsramosarteaga@gmail.com>
2024-06-01 08:41:11 -0500

    Create node server

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 // Importar conexion a db
 const { connection } = require("./database/connection");
-// Importar dependencias
-// const express = require('express');
-// const cors = require('cors');
 
-console.log("Starting...");
+// Importar dependencias
+const express = require('express');
+const cors = require('cors');
+
+console.log("API REST with nodeJs App Started!");
 
 // Ejecutar conecion a bd
 connection();
+
 // Crear servidor de node
+const app = express();
+const port = 3910
 
 // Configurar CORS
+app.use(cors());
 
 // Convertir los datos del body a objetos JS
+app.use(express.urlencoded({extended: true}));
 
 // Cargar configuraciòn rutas
 
 // Ruta de prueba
 
 // Poner el server a escuchar peticiones HTTP
-
+app.listen(port, () => { 
+    console.log(`Server running on port ${port}`);
+});
 
 
