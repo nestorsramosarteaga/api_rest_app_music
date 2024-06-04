7108458e8a147c88017377b920058fa3a6af8f7f
Nestor Ramos <nestorsramosarteaga@gmail.com>
2024-06-04 07:22:04 -0500

    Update start.bat

--- startWiithMongo.bat
+++ startWiithMongo.bat
@@ -1,2 +1,2 @@
 start cmd.exe /k "docker run -d -p 27019:27017 -v C:\data\db:/data/db --name apirest-mongodb mongo:7.0"
-start cmd.exe /k "cd E:\MERN\nodejs\api_rest_app_music && npm start"
+start cmd.exe /k "cd E:\MERN\nodejs\api_rest_app_music && npm run dev"
