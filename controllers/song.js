1b7e7d994a7d861da143eb6b09f5c39143e12b51
Nestor Ramos <nestorsramosarteaga@gmail.com>
2024-06-10 12:49:17 -0500

    Add save method in song controller and router

--- controllers/song.js
+++ controllers/song.js
@@ -1,3 +1,6 @@
+// Import dependecies
+const Song = require("../models/song");
+
 // testing
 const testing = (req,res) => {
     return res.status(200).send({
@@ -5,7 +8,41 @@ const testing = (req,res) => {
         message: "Message sent from song controller"
     });
 }
+
+const save = (req, res) => {
+    // Collection data
+    let params = req.body;
+
+    // Create a object using Song model
+    let song = new Song(params);
+
+    // Save Song
+    song.save().then((songStored) => {
+
+        if(!songStored){
+            return res.status(400).send({
+                status: "error",
+                message: "The song has not been saved"
+            });
+        }
+
+        // Return response
+        return res.status(201).send({
+            status: "success",
+            song: songStored
+        });
+
+    }).catch((error)=>{
+        return res.status(500).send({
+            status: "error",
+            message: "An error occurred while saving the Song",
+            error: error.message
+        });
+    });
+}
+
 // export
 module.exports = {
-    testing
+    testing,
+    save
 }
--- routes/song.js
+++ routes/song.js
@@ -4,11 +4,15 @@ const express = require('express');
 // Load Router
 const router = express.Router();
 
+// Import Middlewares
+const check = require('../middlewares/auth');
+
 // Import Controller
 const SongController = require('../controllers/song');
 
 // Define routes
 router.get('/testing', SongController.testing);
+router.post('/save', check.auth, SongController.save);
 
 // Export router
 module.exports = router;
